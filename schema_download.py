d8bebd5e22d32a3454715b2bfcb539c853faca3a
Anthony Garcia <lagg@lavabit.com>
2013-03-25 16:38:48 -0700

    Cleanup, efficiency changes, better concurrency
    The threading usage is a bit better here, where commits are made
    as the schema sets are pulled from the queue instead of making a big
    ol' dict and looping through it, then adding.
    
    Added a moe reasonable structure to be passed in and out of queues
    (a dict).

--- schema_download.py
+++ schema_download.py
@@ -22,14 +22,15 @@ import threading, Queue
 api_key = None
 language = "en_US"
 fetch_timeout = 5
-games = {"Portal 2": 620,
-         "Team Fortress 2": 440,
-         "Team Fortress 2 Beta": 520,
-         "DOTA 2": 570,
-         "DOTA 2 Alt 1": 816,
-         "DOTA 2 Beta": 205790,
-         "Counter Strike Global Offensive": 730
-         }
+games = {
+        620: "Portal 2",
+        440: "Team Fortress 2",
+        520: "Team Fortress 2 Beta",
+        570: "DOTA 2",
+        816: "DOTA 2 Alt 1",
+        205790: "DOTA 2 Beta",
+        730: "Counter Strike Global Offensive"
+        }
 
 # GIT_WORKING_TREE is where the files are
 tracker_dir = os.path.join(os.getcwd(), "schema-tracking/")
@@ -59,6 +60,9 @@ connection_user_agent = "Lagg/Wiki-Tracker"
 
 # End configuration
 
+# Initial HTTP headers
+http_headers = {"User-Agent": connection_user_agent}
+
 # Different handlers can be set for this, maybe for log display in IRC at some point
 log = logging.getLogger("schema-daemon")
 log.setLevel(logging.INFO)
@@ -69,8 +73,7 @@ log_handler.setFormatter(logging.Formatter("%(levelname)s:\t %(message)s"))
 log.addHandler(log_handler)
 
 # Keeps track of last-modified stamps for both schemas
-api_lm_store = {}
-client_lm_store = {}
+last_modified_store = {}
 
 # Caches items_game URLs
 client_schema_urls = {}
@@ -100,7 +103,32 @@ if not os.path.exists(tracker_dir):
     run_git("add", "-A")
     run_git("commit", "-m", "Origin")
 
-ts_logfmt = "{0:<" + str(sorted(map(len, games.keys()), reverse = True)[0]) + "} {1}"
+def normalize_schema_data(data):
+    return data.replace("\r\n", '\n').replace('\r', '\n')
+
+def fetch_normalized(url, lm = None):
+    data = None
+    code = None
+
+    try:
+        req = urllib2.Request(url, headers = http_headers)
+
+        if lm:
+            req.add_header("If-Modified-Since", lm)
+
+        response = urllib2.urlopen(req, timeout = fetch_timeout)
+        lm = response.headers.get("last-modified")
+        code = response.code
+        data = normalize_schema_data(response.read())
+    except urllib2.HTTPError as E:
+        code = E.getcode()
+    except Exception as E:
+        log.warning("Unexpected error: " + repr(E))
+
+    log.debug("{0}: Code: {1} - LM: {2}".format(url, code, lm))
+
+    return data, lm, code
+
 class download_thread(threading.Thread):
     def __init__(self, inq, outq):
 	super(download_thread, self).__init__()
@@ -109,32 +137,40 @@ class download_thread(threading.Thread):
 
     def run(self):
         while True:
-            appname, url, lm = self.inq.get()
+            out = self.inq.get()
+            app = out["app"]
 
-            req = urllib2.Request(url)
-            req.add_header("User-Agent", connection_user_agent)
+            url = "http://api.steampowered.com/IEconItems_{0}/GetSchema/v1/?key={1}&language={2}".format(app, api_key, language)
+
+            log.info("API:{0}: Start".format(app))
+
+            clienturl = out.get("client-url")
+            content, lm, code = fetch_normalized(url, out["last-modified"]["api"])
+
+            log.info("API:{0}: End ({1})".format(app, code))
+
+            if content:
+                clienturl = json.loads(content)["result"]["items_game_url"]
+                out["client-url"] = clienturl
+                out["schema"]["api"] = content
+
             if lm:
-                req.add_header("If-Modified-Since", lm)
-            content = ''
+                out["last-modified"]["api"] = lm
 
-            try:
-                log.info("Starting " + appname)
-                response = urllib2.urlopen(req, timeout = fetch_timeout)
-                content = response.read().replace("\r\n", '\n').replace('\r', '\n')
-                log.info("Ending " + appname)
-                lm = response.headers.get("last-modified", "never")
-                log.info("New: " + ts_logfmt.format(appname, lm))
-            except urllib2.HTTPError as E:
-                code = E.getcode()
+            if clienturl:
+                log.info("Client:{0}: Start".format(app))
 
-                if code == 304:
-                    log.info("Old: " + ts_logfmt.format(appname, lm))
-                else:
-                    log.error("HTTP {0} received".format(code))
-            except Exception as E:
-                log.error("Unknown error: " + repr(E))
+                content, lm, code = fetch_normalized(clienturl, out["last-modified"]["client"])
 
-            self.outq.put((appname, content, lm))
+                if content:
+                    out["schema"]["client"] = content
+
+                if lm:
+                    out["last-modified"]["client"] = lm
+
+                log.info("Client:{0}: End ({1})".format(app, code))
+
+            self.outq.put(out)
 
             self.inq.task_done()
 
@@ -146,133 +182,92 @@ for i in range(connection_pool_size):
     t.daemon = True
     t.start()
 
-def download_urls(urls, lm_store):
-    body = {}
+def download_schemas():
+    for app, name in games.iteritems():
+        inobj = {
+                "app": app,
+                "client-url": client_schema_urls.get(app),
+                "schema": {
+                    "api": None,
+                    "client": None,
+                    },
+                "last-modified": last_modified_store.setdefault(app, {"api": None, "client": None})
+                }
 
-    for appname, url  in urls:
-        inqueue.put((appname, url, lm_store.get(appname)))
+        inqueue.put(inobj)
 
-    expected = len(urls)
+    expected = len(games)
     received = 0
     maxtries = 5
     usedtries = 0
 
     while usedtries < maxtries and received < expected:
         try:
-            app, content, lm = outqueue.get(timeout = 5)
-            body[app] = content
-            lm_store[app] = lm
+            schema = outqueue.get(timeout = 10)
             received += 1
             usedtries = 0
+
+            app = schema["app"]
+            name = games[app]
+            idealbranch = get_ideal_branch_name(name)
+
+            apibasename = name + " Schema"
+            clientbasename = name + " Client Schema"
+
+            run_git("branch", idealbranch, "master")
+            run_git("checkout", idealbranch)
+
+            summary = {}
+            clienturl = schema["client-url"]
+            apidata = schema["schema"]["api"]
+            clientdata = schema["schema"]["client"]
+            last_modified = schema["last-modified"]
+            apits = last_modified["api"]
+            clientts = last_modified["client"]
+
+            if apits:
+                last_modified_store[app]["api"] = apits
+
+            if clientts:
+                last_modified_store[app]["client"] = clientts
+
+            if clienturl:
+                client_schema_urls[app] = clienturl
+
+            if apidata:
+                with open(os.path.join(tracker_dir, apibasename), "wb") as out:
+                    out.write(apidata)
+                    summary["API"] = apits or "N/A"
+
+            if clientdata:
+                with open(os.path.join(tracker_dir, clientbasename), "wb") as out:
+                    out.write(clientdata)
+                    summary["Client"] = clientts or "N/A"
+
+            commit_header = ", ".join(summary.keys()) or "None (wait what?)"
+            commit_body = '\n\n'.join([type + ": " + ts for type, ts in summary.iteritems()])
+
+            run_git("add", apibasename, clientbasename)
+            run_git("commit", "-m", commit_header + "\n\n" + commit_body)
         except Queue.Empty:
             usedtries += 1
 
     if received != expected:
         log.error("Expected {0} - Got {1} ({2}/{3} retries used)".format(expected, received, usedtries, maxtries))
 
-    return body
-
 def get_ideal_branch_name(label):
     return label.replace(' ', '').lower()
 
-urls = []
-for k, v in games.iteritems():
-    url = (k, "http://api.steampowered.com/IEconItems_{0}/GetSchema/v0001/?key={1}&language={2}".format(v, api_key, language))
-    urls.append(url)
-
 while True:
-    clienturls = []
-    commit_summary = {}
-    schemadata = {}
-
-    log.info("Downloading API schemas...")
-    body = download_urls(urls, api_lm_store)
-
-    for k, v in body.iteritems():
-        schema_base_name = k + " Schema"
-        schema_path = os.path.join(tracker_dir, schema_base_name)
-
-        try:
-            commit_summary[k] = {schema_base_name: api_lm_store[k]}
-            schemadict = None
-            res = str(v)
-            olddata = None
-
-            run_git("checkout", get_ideal_branch_name(k))
-            if os.path.exists(schema_path):
-                with open(schema_path, "rb") as fs:
-                    olddata = fs.read()
-
-            if res:
-                schemadict = json.loads(res)
-                schemadata[schema_base_name] = res
-            elif k not in client_schema_urls and olddata:
-                schemadict = json.loads(olddata)
-
-            if schemadict:
-                client_schema_urls[k] = schemadict["result"]["items_game_url"]
-
-            clienturls.append((k, client_schema_urls[k]))
-        except Exception as e:
-            log.error("Failing API schema write softly: " + str(e))
-
-    clientbody = {}
-    # Don't bother if client url list is empty
-    if clienturls:
-        log.info("Downloading client schemas...")
-        clientbody = download_urls(clienturls, client_lm_store)
-
-    for k, v in clientbody.iteritems():
-        res = str(v)
-        schema_base_name = k + " Client Schema"
-
-        if res:
-            schemadata[schema_base_name] = res
-
-        if k not in commit_summary: commit_summary[k] = {}
-
-        commit_summary[k][schema_base_name] = client_lm_store[k]
-
-    pushready = False
-    for game, summary in commit_summary.iteritems():
-        ideal_branch_name = get_ideal_branch_name(game)
-        files = summary.keys()
-
-        # Checkout branch
-        run_git("branch", ideal_branch_name, "master")
-        run_git("checkout", ideal_branch_name)
-
-        # Write schemas
-        validfiles = []
-        for f in files:
-            if f in schemadata:
-                validfiles.append(f)
-                path = os.path.join(tracker_dir, f)
-                fstream = open(path, "wb")
-                fstream.write(schemadata[f])
-                fstream.close()
-                del schemadata[f]
-
-        summary_top = ", ".join(validfiles)
-        summary_body = "\n\n".join(["{0}: {1}".format(k, v) for k, v in summary.iteritems()])
-        if summary_top:
-            pushready = True
-
-            log.info("Committing: " + summary_top)
-            log.debug(summary_body)
-
-            # Add current game files
-            run_git("add", *validfiles)
-
-            # Commit all (just to make sure)
-            run_git("commit", "-m", summary_top + "\n\n" + summary_body + "\n")
-        else:
-            log.info("Nothing changed for " + game)
+    download_schemas()
 
     # Poosh leetle tracker tree (if push URL is set)
     if tracker_push_url and pushready:
         log.info("Pushing commits...")
         run_git("push", "--porcelain", "--all", tracker_push_url)
+
+    log.debug("LM: " + str(last_modified_store))
+    log.debug("URL Cache: " + str(client_schema_urls))
 
     log.info("Sleeping for {0} second(s)".format(schema_check_interval))
     time.sleep(schema_check_interval)
